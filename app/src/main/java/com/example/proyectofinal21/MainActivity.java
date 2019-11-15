c41281dafd786ffa00b0e5ab9b50a6bcb2b824be
lizita931 <lopezarely930@gmail.com>
2019-11-15 10:29:58 -0600

    Mnesaje de Advertencia y banderas para saber estado

--- app/src/main/java/com/example/proyectofinal21/MainActivity.java
+++ app/src/main/java/com/example/proyectofinal21/MainActivity.java
@@ -67,7 +67,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
