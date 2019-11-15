eac9fbd7dc1c728dd5ade979309b9032981dcd89
kevin2228 <kevin22alexander28@gmail.com>
2019-11-15 00:19:18 -0600

    Agregue la clase Dto

---
+++ app/src/main/java/com/example/proyectofinal21/Dto.java
@@ -0,0 +1,58 @@
+package com.example.proyectofinal21;
+
+public class Dto {
+
+    int codigo;
+    String autor;
+    String letra;
+    String genero;
+    String nombre;
+
+    public Dto(int codigo, String autor, String letra, String genero, String nombre) {
+        this.codigo = codigo;
+        this.autor = autor;
+        this.letra = letra;
+        this.genero = genero;
+        this.nombre = nombre;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getLetra() {
+        return letra;
+    }
+
+    public void setLetra(String letra) {
+        this.letra = letra;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
