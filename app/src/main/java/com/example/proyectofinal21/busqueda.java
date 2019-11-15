374eef410c121c662350577a0d73229a7cbcc25d
Eduarfer <Eduar.fer.nek@gmail.com>
2019-11-15 10:36:16 -0600

    Modifique la clase busqueda

--- app/src/main/java/com/example/proyectofinal21/busqueda.java
+++ app/src/main/java/com/example/proyectofinal21/busqueda.java
@@ -2,13 +2,31 @@ package com.example.proyectofinal21;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.Bundle;
 
-public class busqueda extends AppCompatActivity {
+public class busqueda {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_busqueda);
+    Dto datos = new Dto();
+
+    boolean estadoGuardar = false;
+    boolean estadoEliminar = false;
+
+    private ProgressDialog pd;
+    AlertDialog.Builder dialogo1;
+    AlertDialog.Builder dialogo;
+    ProgressDialog progressDialog;
+
+    //List<Productos> productosList;
+    //List<> productosList;
+    ProductsAdapter adapter;
+
+    public void consultarDescripcion(final Context context, final String autor){
+
+
     }
-}
+
+    }
+
