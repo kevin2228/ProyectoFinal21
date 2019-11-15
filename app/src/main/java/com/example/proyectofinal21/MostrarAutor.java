2ab96292676e4691ff04a711ac9f9eefbb39a1ac
Kevin2228 <kevin22alexander28@gmail.com>
2019-11-15 10:15:52 -0600

    Adicione la clase MostrarAutor

---
+++ app/src/main/java/com/example/proyectofinal21/MostrarAutor.java
@@ -0,0 +1,14 @@
+package com.example.proyectofinal21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MostrarAutor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_autor);
+    }
+}
