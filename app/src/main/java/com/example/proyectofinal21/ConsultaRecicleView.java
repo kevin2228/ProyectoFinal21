06566c124742d9d52f7b7dfea2966c572b77acb9
Eduarfer <eduar.fer.nek@gmail.com>
2019-11-17 11:32:40 -0600

    Modifique el onCreate en consultaRecicleView

--- app/src/main/java/com/example/proyectofinal21/ConsultaRecicleView.java
+++ app/src/main/java/com/example/proyectofinal21/ConsultaRecicleView.java
@@ -2,12 +2,17 @@ package com.example.proyectofinal21;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConsultaRecicleView extends AppCompatActivity {
@@ -47,5 +52,35 @@ public class ConsultaRecicleView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consulta_recicle_view);
+
+        Toolbar toolbar = findViewById(R.id.toolbar1);
+        toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_back));
+        toolbar.setTitleTextColor(getResources().getColor(R.color.mycolor1));
+        toolbar.setTitleMargin(0, 0, 0, 0);
+        toolbar.setSubtitle("Lista de Alabanzas y Coros");
+        toolbar.setSubtitleTextColor(getResources().getColor(R.color.mycolor));
+        toolbar.setTitle("Himnario");
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogConfirmacion();
+            }
+        });
+
+        ///y esto para pantalla completa (oculta incluso la barra de estado)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        productosList = new ArrayList<>();
+
+
+
+        //Toast.makeText(this, "Si", Toast.LENGTH_SHORT).show();
+
+        loadProductos();
     }
 }
