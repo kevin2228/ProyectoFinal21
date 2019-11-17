7711446db64fe3a5acbe55db8cf1b23a0983ce7b
Eduarfer <eduar.fer.nek@gmail.com>
2019-11-17 14:47:37 -0600

    Adicione el metdodo para cargar los himnarios

--- app/src/main/java/com/example/proyectofinal21/ConsultaRecicleView.java
+++ app/src/main/java/com/example/proyectofinal21/ConsultaRecicleView.java
@@ -11,6 +11,16 @@ import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -85,5 +95,61 @@ public class ConsultaRecicleView extends AppCompatActivity {
 
     }
 
+    private void loadProductos() {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        //Toast.makeText(Consulta_RecyclerView.this, ""+response, Toast.LENGTH_SHORT).show();
+
+                        try {
+                            JSONArray array = new JSONArray(response);
+                            int totalEncontrados = array.length();
+                            Toast.makeText(ConsultaRecicleView.this, "Total: "+totalEncontrados, Toast.LENGTH_SHORT).show();
+
+                            for (int i = 0; i < array.length(); i++) {
+
+                                JSONObject articulosObject = array.getJSONObject(i);
+
+                                //String img = articulosObject.getString("imagen");
+                                //Toast.makeText(Consulta_RecyclerView.this, ""+img, Toast.LENGTH_SHORT).show();
+
+                                /*int codigo = articulosObject.getInt("codigo");
+                                String descripcion = articulosObject.getString("descripcion");
+                                double precio = articulosObject.getDouble("precio");
+                                String img = articulosObject.getString("imagen");
+                                Productos objeto = new Productos(codigo, descripcion, precio, img);
+                                productosList.add(objeto);*/
+
+                                productosList.add(new Productos(
+                                        articulosObject.getInt("codigo"),
+                                        articulosObject.getString("letra"),
+                                        articulosObject.getString("autor"),
+                                        articulosObject.getString("nombre"),
+                                        articulosObject.getString("genero")
+                                ));
+                            }
+
+                            adapter = new ProductsAdapter(ConsultaRecicleView.this, productosList);
+                            recyclerView.setAdapter(adapter);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(ConsultaRecicleView.this, "Error. Compruebe su acceso a Internet.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Volley.newRequestQueue(this).add(stringRequest);
+        // MySingleton.getInstance(this).addToRequestQueue(stringRequest);
+        MySingleton.getInstance(ConsultaRecicleView.this).addToRequestQueue(stringRequest);
     }
+
+
 }
+
