9554938be19031f4ca422c389adaf01d1dec14d3
kevin2228 <kevin22alexander28@gmail.com>
2019-11-17 16:51:12 -0600

    Adicione la clase NombreAutor

---
+++ app/src/main/java/com/example/proyectofinal21/NombreAutor.java
@@ -0,0 +1,14 @@
+package com.example.proyectofinal21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NombreAutor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nombre_autor);
+    }
+}
