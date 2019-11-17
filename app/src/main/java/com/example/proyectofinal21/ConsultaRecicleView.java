13d604b0b0e9ce886f33c83af0de10033c0d1aaa
Eduarfer <eduar.fer.nek@gmail.com>
2019-11-17 11:30:29 -0600

    Adicione el metodo de onKeyDown

--- app/src/main/java/com/example/proyectofinal21/ConsultaRecicleView.java
+++ app/src/main/java/com/example/proyectofinal21/ConsultaRecicleView.java
@@ -1,10 +1,47 @@
 package com.example.proyectofinal21;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
+
+import java.util.List;
 
 public class ConsultaRecicleView extends AppCompatActivity {
+
+    private static final String URL = Config.urlConsultaApiMySQLi;
+
+    List<Productos> productosList;
+    RecyclerView recyclerView;
+
+    ProductsAdapter adapter;
+
+    AlertDialog.Builder dialogo;
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            new android.app.AlertDialog.Builder(this)
+                    .setIcon(R.drawable.ic_close)
+                    .setTitle("Advertencia")
+                    .setMessage("¿Realmente desea salir?")
+                    .setNegativeButton(android.R.string.cancel, null)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {//un listener que al pulsar, cierre la aplicacion
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .show();
+            return true;
+        }
+        //para las demas cosas, se reenvia el evento al listener habitual
+        return super.onKeyDown(keyCode, event);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
