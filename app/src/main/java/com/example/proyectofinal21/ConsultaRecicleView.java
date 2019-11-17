6fab5121133f15cde0b2055c910167c3e6a52e11
Eduarfer <eduar.fer.nek@gmail.com>
2019-11-17 11:27:40 -0600

    Adicione la activity para la consulta del recicleView

---
+++ app/src/main/java/com/example/proyectofinal21/ConsultaRecicleView.java
@@ -0,0 +1,14 @@
+package com.example.proyectofinal21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConsultaRecicleView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta_recicle_view);
+    }
+}
